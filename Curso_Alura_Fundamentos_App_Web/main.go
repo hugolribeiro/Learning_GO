2c2f95338826eba22644a1821c927fd734fa5e63
hugolribeiro <hugolerib@gmail.com>
2021-08-19 16:04:31 -0300

    Conectando com banco de dados

--- Curso_Alura_Fundamentos_App_Web/main.go
+++ Curso_Alura_Fundamentos_App_Web/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"database/sql"
+	_ "github.com/lib/pq"
 	"html/template"
 	"net/http"
 )
 
+func conectaComBancoDeDados() *sql.DB {
+	conexao := "user=postgres dbname=alura_loja password=suasenha host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -21,11 +33,36 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39.00, Quantidade: 5},
-		{"Tenis", "Confortável", 89.99, 3},
-		{"Fone", "Muito bom", 59.50, 2},
-		{"Produto novo", "Muito legal", 1.99, 1},
+	db := conectaComBancoDeDados()
+
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+	if err != nil {
+		panic(err.Error())
 	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	// lê linha a linha
+	for selectDeTodosOsProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Id = id
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+	}
+
 	templates.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
 }
