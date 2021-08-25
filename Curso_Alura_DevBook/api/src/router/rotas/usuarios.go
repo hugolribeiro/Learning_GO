1a2f6634bd72cc3164c8762c7d9004f2a92dfcff
hugolribeiro <hugolerib@gmail.com>
2021-08-24 22:42:03 -0300

    feat: Configuradas todas as rotas iniciais do projeto DevBook

---
+++ Curso_Alura_DevBook/api/src/router/rotas/usuarios.go
@@ -0,0 +1,39 @@
+package rotas
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var rotasUsuarios = []Rota{
+	{ //cadastra um usuario
+		URI:                "/usuarios",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.CriarUsuario,
+		RequerAutenticacao: false,
+	},
+	{ //busca todos os usuarios
+		URI:                "/usuarios",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarUsuarios,
+		RequerAutenticacao: false,
+	},
+	{ // Busca um usuário por Id
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarUsuario,
+		RequerAutenticacao: false,
+	},
+	{ // Atualiza um usuário
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodPut,
+		Funcao:             controllers.AtualizarUsuario,
+		RequerAutenticacao: false,
+	},
+	{ //deleta um usuario
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodDelete,
+		Funcao:             controllers.DeletarUsuario,
+		RequerAutenticacao: false,
+	},
+}
