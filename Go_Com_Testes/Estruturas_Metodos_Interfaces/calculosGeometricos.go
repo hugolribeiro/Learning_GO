c06eaf103bbe998696f3cff19ec8f4743349bf85
hugolribeiro <hugolerib@gmail.com>
2021-09-08 11:29:12 -0300

    Trabalhando com interfaces para calcular area de formas

---
+++ Go_Com_Testes/Estruturas_Metodos_Interfaces/calculosGeometricos.go
@@ -0,0 +1,32 @@
+package estruturasmetodosinterfaces
+
+import "math"
+
+type Forma interface {
+	Area() float64
+}
+
+type Retangulo struct {
+	Largura float64
+	Altura  float64
+}
+
+func (r Retangulo) Area() float64 {
+	return r.Largura * r.Altura
+}
+
+type Circulo struct {
+	Raio float64
+}
+
+func (c Circulo) Area() float64 {
+	return math.Pi * math.Pow(c.Raio, 2)
+}
+
+func Perimetro(retangulo Retangulo) float64 {
+	return 2 * (retangulo.Largura + retangulo.Altura)
+}
+
+func Area(retangulo Retangulo) float64 {
+	return retangulo.Largura * retangulo.Altura
+}
