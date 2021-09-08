9442bb7465e4847913faf6c72387aa71f0e40907
hugolribeiro <hugolerib@gmail.com>
2021-09-08 11:36:37 -0300

    Adicionando forma de table driven tests

--- Go_Com_Testes/Estruturas_Metodos_Interfaces/calculosGeometricos_test.go
+++ Go_Com_Testes/Estruturas_Metodos_Interfaces/calculosGeometricos_test.go
@@ -16,27 +16,18 @@ func TestPerimetro(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	verificaArea := func(t *testing.T, forma Forma, esperado float64) {
-		t.Helper()
-		resultado := forma.Area()
-
-		if resultado != esperado {
-			t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
-		}
+	testesArea := []struct {
+		forma    Forma
+		esperado float64
+	}{
+		{forma: Retangulo{Largura: 12, Altura: 6}, esperado: 72.0},
+		{forma: Circulo{10}, esperado: 314.1592653589793},
 	}
 
-	t.Run("retângulos", func(t *testing.T) {
-		retangulo := Retangulo{Largura: 12.0, Altura: 6.0}
-		esperado := 72.0
-		verificaArea(t, retangulo, esperado)
-
-	})
-
-	t.Run("círculos", func(t *testing.T) {
-		circulo := Circulo{10}
-		esperado := 314.1592653589793
-		verificaArea(t, circulo, esperado)
-
-	})
-
+	for _, tt := range testesArea {
+		resultado := tt.forma.Area()
+		if resultado != tt.esperado {
+			t.Errorf("resultado %.2f, esperado %.2f", resultado, tt.esperado)
+		}
+	}
 }
