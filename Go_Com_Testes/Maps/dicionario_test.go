565392eae887ffa6c6cde1382e2a4783e9a221cf
hugolribeiro <hugolerib@gmail.com>
2021-09-10 11:40:35 -0300

    Maps - Adicionando funcao ADICIONA e testes

--- Go_Com_Testes/Maps/dicionario_test.go
+++ Go_Com_Testes/Maps/dicionario_test.go
@@ -7,10 +7,28 @@ import (
 func TestBusca(t *testing.T) {
 	dicionario := Dicionario{"teste": "isso é apenas um teste"}
 
-	resultado := dicionario.Busca("teste")
-	esperado := "isso é apenas um teste"
+	t.Run("palavra conhecida", func(t *testing.T) {
+		resultado, _ := dicionario.Busca("teste")
+		esperado := "isso é apenas um teste"
 
-	comparaStrings(t, resultado, esperado)
+		comparaStrings(t, resultado, esperado)
+	})
+
+	t.Run("palavra desconhecida", func(t *testing.T) {
+		_, resultado := dicionario.Busca("desconhecida")
+
+		comparaErro(t, resultado, ErrNaoEncontrado)
+	})
+}
+
+func TestAdiciona(t *testing.T) {
+	dicionario := Dicionario{}
+	palavra := "teste"
+	definicao := "isso é apenas um teste"
+
+	dicionario.Adiciona(palavra, definicao)
+	comparaDefinicao(t, dicionario, palavra, definicao)
+
 }
 
 func comparaStrings(t *testing.T, resultado, esperado string) {
@@ -18,5 +36,26 @@ func comparaStrings(t *testing.T, resultado, esperado string) {
 
 	if resultado != esperado {
 		t.Errorf("resultado '%s', esperado '%s', dado '%s'", resultado, esperado, "test")
+	}
+}
+
+func comparaErro(t *testing.T, resultado, esperado error) {
+	t.Helper()
+
+	if resultado != esperado {
+		t.Errorf("resultado erro '%s', esperado '%s'", resultado, esperado)
+	}
+}
+
+func comparaDefinicao(t *testing.T, dicionario Dicionario, palavra, definicao string) {
+	t.Helper()
+
+	resultado, err := dicionario.Busca(palavra)
+	if err != nil {
+		t.Fatal("deveria ter encontrado palavra adicionada:", err)
+	}
+
+	if definicao != resultado {
+		t.Errorf("resultado  '%s', esperado '%s'", resultado, definicao)
 	}
 }
