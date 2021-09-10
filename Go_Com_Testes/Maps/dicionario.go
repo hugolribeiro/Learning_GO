98386c935b5acfaed0e8acf9f61d1221e471786e
hugolribeiro <hugolerib@gmail.com>
2021-09-10 11:49:46 -0300

    Maps - Verifica se a palavra já existe antes de Adicionar

--- Go_Com_Testes/Maps/dicionario.go
+++ Go_Com_Testes/Maps/dicionario.go
@@ -1,10 +1,17 @@
 package maps
 
-import "errors"
-
 type Dicionario map[string]string
 
-var ErrNaoEncontrado = errors.New("não foi possível encontrar a palavra que você procura")
+const (
+	ErrNaoEncontrado    = ErrDicionario("não foi possível encontrar a palavra que você procura")
+	ErrPalavraExistente = ErrDicionario("não é possível adicionar a palavra pois ela já existe")
+)
+
+type ErrDicionario string
+
+func (e ErrDicionario) Error() string {
+	return string(e)
+}
 
 func (d Dicionario) Busca(palavra string) (string, error) {
 	definicao, existe := d[palavra]
@@ -15,6 +22,15 @@ func (d Dicionario) Busca(palavra string) (string, error) {
 	return definicao, nil
 }
 
-func (d Dicionario) Adiciona(palavra string, definicao string) {
-	d[palavra] = definicao
+func (d Dicionario) Adiciona(palavra string, definicao string) error {
+	_, err := d.Busca(palavra)
+	switch err {
+	case ErrNaoEncontrado:
+		d[palavra] = definicao
+	case nil:
+		return ErrPalavraExistente
+	default:
+		return err
+	}
+	return nil
 }
