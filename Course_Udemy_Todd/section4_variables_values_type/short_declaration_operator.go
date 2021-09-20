50725bac2ff083bd2702fd30c194428c21afc745
hugolribeiro <hugolerib@gmail.com>
2021-09-20 10:54:58 -0300

    adding zero value and scope

--- Course_Udemy_Todd/section4_variables_values_type/short_declaration_operator.go
+++ Course_Udemy_Todd/section4_variables_values_type/short_declaration_operator.go
@@ -2,13 +2,25 @@ package main
 
 import "fmt"
 
+// global scope
+var y = 43
+
+// DECLARE there is a VARIABLE with the IDENTIFIER "z"
+// and that the VARIABLE with the IDENTIFIER "z" if of TYPE int
+
+var z int
+
 func main() {
+	// short declaration operator
+	// DECLARE a variable and ASSIGN a VALUE (of a certain TYPE)
+	fmt.Println(z)
 	x := 42
 	fmt.Println(x)
 	x = 99
 	fmt.Println(x)
-	y := 100 + 70
 	fmt.Println(y)
-	z := "Bond, James"
-	fmt.Println(z)
+	y = 100 + 70
+	fmt.Println(y)
+	w := "Bond, James"
+	fmt.Println(w)
 }
